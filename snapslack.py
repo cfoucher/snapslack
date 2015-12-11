e7d61445174251635f419c459fc1b5d3ad3a4e09
Conrad Foucher <conrad@foucher.ca>
2015-12-11 15:51:07 -0500

    Fix race conditions between update and delete. Messages are now reliably delete!

--- snapslack.py
+++ snapslack.py
@@ -1,12 +1,11 @@
 from flask import Flask, request, render_template
+from multiprocessing import Process
 import sys
 import redis
 import json
 import requests
 import requests.packages.urllib3
 import time
-import threading
-import multiprocessing
 
 requests.packages.urllib3.disable_warnings()
 
@@ -23,7 +22,7 @@ def landingPage():
 
 @app.route('/test')
 def test():
-    return 'OK'
+    return render_template('error.html')
 
 @app.route('/slash', methods=['POST'])
 def slashCommand():
@@ -35,13 +34,21 @@ def slashCommand():
     if not token:
         return "Looks like you don't have Snapslack enabled for your account yet. Please visit https://snapslack.conradfoucher.ca to enable it."
 
-    r = requests.post(app.config['SLACK_API_BASE']+'chat.postMessage', data = {"token":token, "channel":request.form['channel_id'], "text":"*Snapslack-`10`*: " + request.form['text'], "username":'SS - '+request.form['user_name'], "as_user":'true'})
-    r = r.json()
+    if request.form['text']:
+        r = requests.post(app.config['SLACK_API_BASE']+'chat.postMessage', data = {"token":token, "channel":request.form['channel_id'], "text":"*Snapslack-`10`*: " + request.form['text'], "username":'SS - '+request.form['user_name'], "as_user":'true'})
+        r = r.json()
 
-    if r['ok']:
-        snapData = {"channel_id": request.form['channel_id'], "ts":r['ts'], "team_id":request.form['team_id'], "text":request.form['text'], "user_id":request.form['user_id']}
-        db.zadd('snaps', str(time.time()), json.JSONEncoder().encode(snapData))
-    return ''
+        if r['ok']:
+            snapData = {"channel_id": request.form['channel_id'], "ts":r['ts'], "team_id":request.form['team_id'], "text":request.form['text'], "user_id":request.form['user_id']}
+            db.zadd('snaps', str(time.time()), json.JSONEncoder().encode(snapData))
+            return ''
+        else:
+            if r['error'] == 'invalid_auth' or r['error'] == 'not_authed' or r['error'] == 'account_inactive':
+                return "Looks like you don't have Snapslack enabled for your account yet. Please visit https://snapslack.conradfoucher.ca to enable it."
+            else:
+                return "Something went wrong. Please try again later. If the issue persists please try reauthenticating at https://snapslack.conradfoucher.ca"
+    else:
+        return "You need to type something in order to Snapslack!"
 
 
 @app.route('/oauth', )
@@ -64,56 +71,86 @@ def oauth():
         db.set(r['user_id']+'-'+team_id, access_token)
     else:
         r.raise_for_status()
+        return render_template('error.html')
     
-    return "Successfully Authenticated!"
+    return render_template('authsuccess.html')
 
-#@app.before_first_request
-#def snapProcessing():
-#    print "yo"
-#    #p = multiprocessing.Process(target=updateSnaps())
-#    #p.start()
-#    return 'ok'
-#    #updateSnaps()
+@app.before_first_request
+def snapProcessing():
+    print "Starting Snapslack Process Workers"
+    updateProcess = Process(target=updateCountdowns)
+    removeProcess = Process(target=removeSnaps)
+    updateProcess.start()
+    removeProcess.start()
+    return 'Processing workers are Running'
 
-def updateSnaps():
+
+def updateCountdowns():
 
     currentTime = time.time()
     expireTime = currentTime - 10
-    snapsToUpdate = db.zrangebyscore('snaps', '-inf', '+inf', withscores=True)
+    # Grab all snaps from db that need to have their countdowns updated. 
+    snapsToUpdate = db.zrangebyscore('snaps', '('+str(expireTime), '+inf', withscores=True)
 
     for snap in snapsToUpdate:
         snapData = json.loads(snap[0])
         count = int(10 - round(currentTime-float(snap[1])))
-        if(count <= 0):
-            sys.stdout.flush()
-            deleted = delete(snapData['user_id'], snapData['team_id'], snapData['ts'], snapData['channel_id'])
-            if deleted:
-                db.zrem('snaps', snap[0])
+        if(count > 0):
+            # Remove Snap from 'snaps' list to prevent race condition between delete requests and update requests. Make a backup of it at the same time.
+            pipe = db.pipeline()
+            removeAndBackup = pipe.zrem('snaps', snap[0]).set('tmp-Update-'+str(snap[1]), snap[0]).execute()
+            if(removeAndBackup[0] > 0):
+                # We grabed it before the delete process -> update it
+                r = requests.post(app.config['SLACK_API_BASE']+'chat.update', data = {"token":db.get(snapData['user_id']+'-'+snapData['team_id']), "ts":snapData['ts'], "channel":snapData['channel_id'], "text":"*Snapslack-`"+str(count)+"`:* " + snapData['text']}, timeout=5)
+                # Remove the tmp-backup entry and add it back to the 'snaps' list
+                pipe = db.pipeline()
+                removeAndAdd = pipe.delete('tmp-Update-'+str(snap[1])).zadd('snaps', snap[1], snap[0]).execute()
+                if(removeAndAdd[1] < 1):
+                    # Couldn't add the snap back to the list. Removing it from slack!
+                    deleted = deleteSnap(snapData['user_id'], snapData['team_id'], snapData['ts'], snapData['channel_id'])
             else:
-                print ('message did not successfully delete')
-                sys.stdout.flush()
-        else:
-            r = requests.post(app.config['SLACK_API_BASE']+'chat.update', data = {"token":db.get(snapData['user_id']+'-'+snapData['team_id']), "ts":snapData['ts'], "channel":snapData['channel_id'], "text":"*Snapslack-`"+str(count)+"`:* " + snapData['text']}, timeout=2)
+                # Looks like the delete process grabed it first -> delete tmp backup
+                db.delete('tmp-Update-'+str(snap[1]))
+    time.sleep(1)
+    updateCountdowns()
+
+def removeSnaps():
+
+    currentTime = time.time()
+    expireTime = currentTime - 10
+    # Grab all snaps from db that need to be removed from slack
+    snapsToRemove = db.zrangebyscore('snaps', '-inf', str(expireTime), withscores=True)
+
+    for snap in snapsToRemove:
+        snapData = json.loads(snap[0])
+        pipe = db.pipeline()
+        removeAndBackup = pipe.zrem('snaps', snap[0]).set('tmp-Remove-'+str(snap[1]), snap[0]).execute()
+        if(removeAndBackup[0] > 0):
+            # Successfully removed it from the db. Update Process didn't get it :)
+            deleted = deleteSnap(snapData['user_id'], snapData['team_id'], snapData['ts'], snapData['channel_id'])
+            if deleted:
+                # Snap successfully removed from slack -> delete backup
+                db.delete('tmp-Remove-'+str(snap[1]))
+            else:
+                # Remove the tmp-backup entry and add it back to the 'snaps' list so that we can try deleting it next round
+                pipe = db.pipeline()
+                removeAndAdd = pipe.delete('tmp-Remove-'+str(snap[1])).zadd('snaps', snap[1], snap[0]).execute()
 
     time.sleep(1)
-    updateSnaps()
+    removeSnaps()
 
-def delete(user_id, team_id, ts, channel_id):
-    print ("We will try to delete the message 3 times")
+def deleteSnap(user_id, team_id, ts, channel_id):
+    # We will try to delete the message 3 times
     for x in range(0, 3):
         r = requests.post(app.config['SLACK_API_BASE']+'chat.delete', data = {"token":db.get(user_id+'-'+team_id), "ts":ts, "channel":channel_id}, timeout=2)
         r = r.json()
-
         if r['ok']:
-            print "RESPONSE SAYS MESSAGE IS DELETED!"
-            sys.stdout.flush()
             return True
-
     return False
+
 
 if __name__ == '__main__':
     app.debug = app.config['DEBUG']
     app.run(host='0.0.0.0')
-    print "Starting processing of snaps"
-    p = multiprocessing.Process(target=updateSnaps())
-    p.start()
+    
+
